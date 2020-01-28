96190dd6b98c735ef03f76758614e6cc5ca6f6fb
Luiz Massa <luimassapontes@gmail.com>
2020-01-28 15:42:32 -0300

    Criando o projeto como Node e Instalando o Express como Dependencia.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const http = require('http');
 
-const server = http.createServer(function(request, response){
-    response.end('<h1>Hello World</h1>')
+const server = http.createServer(function(request, response) {
+
+    let html = '';
+    if(request.url == '/') {
+        html = '<h1>Hello World</h1>';
+    } else if (request.url == '/livros') {
+        html = '<h1>Lista de Livros</h1>'
+    }
+
+    response.end(html);
 });
 
 server.listen(3333);
